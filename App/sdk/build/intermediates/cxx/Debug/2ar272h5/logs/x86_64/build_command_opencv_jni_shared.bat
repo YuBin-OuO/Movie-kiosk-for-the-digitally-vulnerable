739af88dcabfe99bb6cc59f5de9f10d7c3516749
YuBin_OuO <jyb424sch@gmail.com>
2023-07-20 17:34:33 +0900

    Initial commit

---
+++ App/sdk/build/intermediates/cxx/Debug/2ar272h5/logs/x86_64/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\CHO_YUBIN\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\CHO_YUBIN\\Desktop\\HH\\Kiosker\\Kiosk\\App\\sdk\\.cxx\\Debug\\2ar272h5\\x86_64" ^
+  opencv_jni_shared
---
+++ App/sdk/build/intermediates/cxx/Debug/2y94v28w/logs/x86/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\CHO_YUBIN\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\CHO_YUBIN\\Desktop\\HH\\Kiosker\\App2\\sdk\\.cxx\\Debug\\2y94v28w\\x86" ^
+  opencv_jni_shared
